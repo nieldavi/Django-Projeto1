9f35c22f94780c3650cf948c8b94b9b2b7a746ee
Francisco Davi <franciscodanielbct@gmail.com>
2022-12-27 14:22:52 -0300

    Removido contanto e sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
 
-from recipes.views import my_contact, my_home, my_On
+from recipes.views import my_home
 
 urlpatterns = [
     path('', my_home),  # home
-    path('sobre/', my_On),
-    path('contato/', my_contact),  # contato
-
 ]

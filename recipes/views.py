11c54ec07bda48cf881b39a1dd9d3a4cf7575aaa
Francisco Davi <franciscodanielbct@gmail.com>
2023-01-05 12:58:13 -0300

    Cards feitos e Arquivos static

--- recipes/views.py
+++ recipes/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 # HTTP request  <- HTTP response
 def my_home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html')

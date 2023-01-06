1157b20e65963dfbaf238b7c4f2486856c74d16c
Francisco Davi <franciscodanielbct@gmail.com>
2023-01-06 18:28:51 -0300

    Templates com Heranças

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 
 from . import views
 
+# recipes:recipe
+
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.my_home),  # home
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.my_home, name="home"),  # home
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 
 ]
